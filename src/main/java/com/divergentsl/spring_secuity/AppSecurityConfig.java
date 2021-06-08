ea106511ec8d46ff921e657f1ef2a42eed6e68df
Sarwesh-divergentsoftlab <sarwesh.chitambare@divergentsl.com>
2021-06-08 16:35:22 +0530

    Added ssl

--- src/main/java/com/divergentsl/spring_secuity/AppSecurityConfig.java
+++ src/main/java/com/divergentsl/spring_secuity/AppSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.divergentsl.spring_secuity;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
--- src/main/java/com/divergentsl/spring_secuity/SpringSecurityApplication.java
+++ src/main/java/com/divergentsl/spring_secuity/SpringSecurityApplication.java
@@ -1,13 +1,45 @@
 package com.divergentsl.spring_secuity;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+import org.apache.catalina.Context;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8082);
+		return connector;
+	}@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		return tomcat;
 	}
 
 }
